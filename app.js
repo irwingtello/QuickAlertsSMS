49b385d801d5a6a9fc04675161df0082aa153a08
Irwing Tello <28800239+irwingtello@users.noreply.github.com>
2023-10-13 08:29:44 -0500

    SMS messages with QuickAlerts

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ app.use(bodyParser.json());
 app.post('/api/quickalerts', (req, res) => {
   // Handle the POST request here
   const requestData = req.body; // This will contain the JSON data from the POST request
-  console.log(requestData);
+
+const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
+
+client.messages
+    .create({
+      body: process.env.MESSAGE,
+      from: process.env.FROM,
+      to:  process.env.TO
+    })
+   // .then(message => console.log(message.sid))
+   .then(message => {console.log(message.sid)})
+    .done();
   // You can process the data and send a response
   res.status(200).json({ message: 'POST request received successfully', data: requestData });
 });
